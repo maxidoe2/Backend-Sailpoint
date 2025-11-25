6d02c15685467815ec2bce9307f158f0588c8661
Máximo Estudillo <92692121+maxidoe2@users.noreply.github.com>
2025-11-24 21:06:41 -0300

    Update AssertivaBackendApplication.java

--- src/main/java/com/assertiva/sailpointlab/assertiva_sailpoint_backend/AssertivaBackendApplication.java
+++ src/main/java/com/assertiva/sailpointlab/assertiva_sailpoint_backend/AssertivaBackendApplication.java
@@ -9,4 +9,13 @@ public class AssertivaBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AssertivaBackendApplication.class, args);
 	}
+	
+	@Bean
+	public FilterRegistrationBean<BasicAuthFilter> authFilter(BasicAuthFilter filter) {
+    	FilterRegistrationBean<BasicAuthFilter> registration = new FilterRegistrationBean<>();
+    	registration.setFilter(filter);
+    	registration.addUrlPatterns("/*"); // Protege toda la API
+    return registration;
+}
+
 }
